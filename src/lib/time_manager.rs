8b72fca6ee2db313c23476c5b4f97cbe0ae282e7
noah figueras <noahfigueras@gmail.com>
2021-11-22 17:15:05 -0800

    Added calculation of time difference on stop()

--- src/lib/time_manager.rs
+++ src/lib/time_manager.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use chrono::prelude::*;
 use ansi_term::{Style, Colour};
-use crate::lib::{PATHP,Task};
+use crate::lib::{PATHP,Task, Tracker};
 use crate::lib::aux::{
     read_json,
     write_json,
@@ -10,7 +10,7 @@ use crate::lib::aux::{
     completed_count,
 };
 
-
+// Start timer for task
 pub fn start(_index: &str) {
     let mut tasks: Vec<Task> = read_json(PATHP).expect("error while reading");
     let index: u8 = _index.parse::<u8>().unwrap();
@@ -26,6 +26,46 @@ pub fn start(_index: &str) {
     write_json(&tasks, PATHP).expect("Unable to write file");
 }
 
+// Stop Timer for task
 pub fn stop(_index: &str) {
-    println!("Stop timer for this task!");
+    let mut tasks: Vec<Task> = read_json(PATHP).expect("error while reading");
+    let index: u8 = _index.parse::<u8>().unwrap();
+    
+    let mut i: u8 = 0;
+    for mut task in &mut tasks {
+        if i == index && task.status.active {
+           task.status.active = false;   
+           let now = Local::now().format("%H:%M").to_string();
+           let _start = &task.status.start;
+           let _tracker = Tracker {
+                    date: Local::now().format("%Y-%m-%d").to_string(),
+                    hours: time_difference(_start.to_string(), now) 
+            };
+
+           // Push tracker info to vector
+           task.status.tracker.push(_tracker);
+        }
+        i=i+1;
+    }
+    write_json(&tasks, PATHP).expect("Unable to write file");
+}
+
+fn time_difference(start_time: String, end_time: String) -> String {
+   // Get hours
+   let  mut end_hour = end_time[..2].parse::<u8>().unwrap(); 
+   let start_hour = start_time[..2].parse::<u8>().unwrap();
+
+   // Get minutes
+   let mut end_min = end_time[3..].parse::<u8>().unwrap();
+   let start_min = start_time[3..].parse::<u8>().unwrap();
+
+   if start_min > end_min {
+       end_hour = end_hour - 1;
+       end_min = end_min + 60;
+   };
+   
+   // Calculate time difference
+   let hours = end_hour - start_hour;
+   let min = end_min - start_min;
+   format!("{}:{}", hours, min).to_string()
 }
